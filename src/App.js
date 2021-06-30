f06e40b040aa88c9bc6a731624ffa8abd5c38ff4
Maksym Honchar <maksim.honchar@gmail.com>
2021-06-30 18:55:23 +0300

    added prettier config file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-import styles from "./app.module.css";
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import styles from './app.module.css'
 
 const useStyles = makeStyles({
   btn: {
-    "&.MuiButton-root": {
-      padding: "6px 16px",
-      textTransform: "lowercase",
+    '&.MuiButton-root': {
+      padding: '6px 16px',
+      textTransform: 'lowercase',
     },
-    "&.MuiButton-contained": {
-      background: "yellow",
+    '&.MuiButton-contained': {
+      background: 'yellow',
     },
-    "&.MuiButton-contained:hover": {
-      background: "green",
-      color: "white",
+    '&.MuiButton-contained:hover': {
+      background: 'green',
+      color: 'white',
     },
   },
-});
+})
 
 function App() {
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
     <div className={styles.root}>
@@ -28,7 +28,7 @@ function App() {
         TITLE BUTTON
       </Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
