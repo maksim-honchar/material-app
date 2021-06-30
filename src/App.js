d34edb2875ea3df38074af12b48a394ab9f00a9a
Maksym Honchar <maksim.honchar@gmail.com>
2021-06-30 17:54:54 +0300

    added .vscode folder

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import styles from  './app.module.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import styles from "./app.module.css";
 
 const useStyles = makeStyles({
   btn: {
-   '&.MuiButton-root': {
-    padding: '6px 16px',
-    textTransform: 'lowercase'
-   },
-   '&.MuiButton-contained': {
-     background: 'yellow'
-   },
-    '&.MuiButton-contained:hover': {
-     background: 'green',
-     color: 'white'
-    }
+    "&.MuiButton-root": {
+      padding: "6px 16px",
+      textTransform: "lowercase",
+    },
+    "&.MuiButton-contained": {
+      background: "yellow",
+    },
+    "&.MuiButton-contained:hover": {
+      background: "green",
+      color: "white",
+    },
   },
 });
 
 function App() {
-  const classes = useStyles()
+  const classes = useStyles();
 
   return (
     <div className={styles.root}>
-       <Button 
-       variant="contained"
-        className={classes.btn}
-       >
-         TITLE BUTTON
-       </Button>
+      <Button variant="contained" className={classes.btn}>
+        TITLE BUTTON
+      </Button>
     </div>
   );
 }
